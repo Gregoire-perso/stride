dfa0b785c1973d86e6e0313c276bc91423720f48
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2023-03-05 21:45:54 +0000

    Rename things
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- app/src/main/java/com/example/stride/ConnectionActivity.java
+++ app/src/main/java/com/example/stride/ConnectionActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ConnexionActivity extends AppCompatActivity {
+public class ConnectionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_connexion);
+        setContentView(R.layout.activity_connection);
     }
 }
