c0766279f90ceac7901606e43ed2a3695d58de63
Fabio Sintoni <sfabio.mail@gmail.com>
2023-03-10 11:40:24 +0000

    Created two activities: Profile and Edit Profile Details

---
+++ app/src/main/java/com/example/stride/EditProfileActivity.java
@@ -0,0 +1,18 @@
+package com.example.stride;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+    }
+
+}
