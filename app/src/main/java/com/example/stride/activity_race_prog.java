a72a73a3e8cc675a8a01eebdc727ed3c97e94794
anna <anna.bony03@gmail.com>
2023-03-28 15:00:28 +0100

    firebase

--- app/src/main/java/com/example/stride/activity_race_prog.java
+++ app/src/main/java/com/example/stride/activity_race_prog.java
@@ -18,11 +18,24 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.Calendar;
 
 public class activity_race_prog extends AppCompatActivity {
     TimePicker picker;
     TimePicker picker2;
+    public FirebaseDatabase database;
+    private DatabaseReference reference;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
     //String newline =System.getProperty("line.separator");
     int hour, minute, hour2, minute2;
     int pm_am, pm_am2;
@@ -36,6 +49,10 @@ public class activity_race_prog extends AppCompatActivity {
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        user = mAuth.getCurrentUser();
+        database = FirebaseDatabase.getInstance("https://stride-99148-default-rtdb.europe-west1.firebasedatabase.app");
+
 
         //CHECK THE CALENDAR PERMISSION
         checkPermission("android.permission.WRITE_CALENDAR", STORAGE_PERMISSION_CODE);
@@ -123,6 +140,12 @@ public class activity_race_prog extends AppCompatActivity {
             if(!title.getText().toString().isEmpty())
             {
                 AddCalendarEvent(year1, month1, day1, hour, hour2, minute);
+
+                //insert a firebase
+                reference = database.getReference().child("Users").child(user.getUid());
+
+
+
             }
 
             else
