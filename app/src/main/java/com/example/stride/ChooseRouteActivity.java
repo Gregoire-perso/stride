19015bf8d97603a44cf42f13f0b9698a15e7ad18
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2023-04-13 18:14:21 +0100

    The route of the user is now tracked
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- app/src/main/java/com/example/stride/ChooseRouteActivity.java
+++ app/src/main/java/com/example/stride/ChooseRouteActivity.java
@@ -1,50 +1,97 @@
 package com.example.stride;
 
+import static android.content.ContentValues.TAG;
+
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.stride.databinding.ActivityChooseRouteBinding;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.ButtCap;
+import com.google.android.gms.maps.model.Cap;
+import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.RoundCap;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.location.FusedLocationProviderClient;
 
-public class ChooseRouteActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
+
+import java.util.List;
+
+public class ChooseRouteActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private MapView mapView;
-    private ActivityChooseRouteBinding binding;
+    private Polyline myTrack;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean locationPermissionGranted = false;
     private LocationManager locationManager;
+    private LocationListener locationListener;
+    private LatLng lastPosition = null;
+    private boolean running = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityChooseRouteBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_choose_route);
 
         mapView = findViewById(R.id.mapView);
-
-        /*
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.mapView);
-        mapFragment.getMapAsync(this);
-
-         */
-        mapView.getMapAsync(this);
         mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        Button startStopButton = findViewById(R.id.startStopButton);
+        startStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (running)
+                    startStopButton.setText(R.string.start_run_button);
+
+                else
+                    startStopButton.setText(R.string.stop_run_button);
+
+                running ^= true;
+
+                setupTrack();
+            }
+        });
+    }
+
+    /**
+     * This will setup the track line with a defined color and joint type shape
+     */
+    private void setupTrack() {
+        myTrack = mMap.addPolyline(new PolylineOptions().clickable(false));
+        myTrack.setJointType(JointType.ROUND);
+        myTrack.setColor(Color.BLUE);
+        myTrack.setStartCap(new RoundCap());
+        myTrack.setEndCap(new ButtCap());
     }
 
     /**
@@ -56,30 +103,95 @@ public class ChooseRouteActivity extends AppCompatActivity implements OnMapReady
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        System.out.println("LOLILOL\n");
+
+        mMap.setMinZoomPreference(1f);
+        mMap.setMaxZoomPreference(20f);
+        // Prompt the user for permission.
+        getLocationPermission();
+
+        // Turn on the My Location layer and the related control on the map.
+        updateLocationUI();
+
+        // Get the current location of the device and set the position of the map.
+        //getDeviceLocation();
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                if (latLng != lastPosition) {
+                    updateCameraPosition(latLng);
+                    drawTrack(latLng);
+                    lastPosition = latLng;
+                }
+            }
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
+    /**
+     * Updates the map's UI settings based on whether the user has granted location permission.
+     */
+    private void updateLocationUI() {
+        if (mMap == null) {
+            return;
+        }
+        try {
+            if (locationPermissionGranted) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+                getLocationPermission();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    private void drawTrack(LatLng latLng) {
+        if (running) {
+            List<LatLng> prevPoints = myTrack.getPoints();
+            prevPoints.add(latLng);
+            myTrack.setPoints(prevPoints);
+        }
+    }
+
+    private void updateCameraPosition(LatLng latLng) {
         if (mMap == null)
             return;
-        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng(latLng);
         mMap.animateCamera(cameraUpdate);
-        locationManager.removeUpdates(this);
     }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) { }
+    /**
+     * Prompts the user for permission to use the device location.
+     */
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
 
-    @Override
-    public void onProviderEnabled(String provider) { }
 
-    @Override
-    public void onProviderDisabled(String provider) { }
+
 
     @Override
     protected void onStart() {
@@ -109,6 +221,7 @@ public class ChooseRouteActivity extends AppCompatActivity implements OnMapReady
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+        locationManager.removeUpdates(locationListener);
     }
 
     @Override
