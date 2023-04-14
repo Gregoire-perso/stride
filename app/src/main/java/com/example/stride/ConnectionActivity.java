162b711347c47684241cfe632a590b242826143e
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2023-04-14 22:19:48 +0100

    Remove useless things in the ConnectionActivity
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- app/src/main/java/com/example/stride/ConnectionActivity.java
+++ app/src/main/java/com/example/stride/ConnectionActivity.java
@@ -4,19 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.firebase.auth.FirebaseAuth;
-
 public class ConnectionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        FirebaseAuth.getInstance().signOut();
     }
 }
