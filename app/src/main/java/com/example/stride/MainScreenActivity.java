5b2adb95c4ed058ee0ebbb14c2e4e9a8c5cb9782
anna <anna.bony03@gmail.com>
2023-04-15 16:54:54 +0100

    main screen almost done (small things left)

--- app/src/main/java/com/example/stride/MainScreenActivity.java
+++ app/src/main/java/com/example/stride/MainScreenActivity.java
@@ -1,9 +1,17 @@
 package com.example.stride;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static java.security.AccessController.getContext;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -39,37 +47,72 @@ public class MainScreenActivity extends AppCompatActivity {
 
         reference = database.getReference().child("Users").child(user.getUid());
         ValueEventListener postListener = new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // Get Post object and use the values to update the UI
                 us = (User)dataSnapshot.getValue(User.class);
+                ZoneId zoneId = ZoneId.systemDefault(); // or: ZoneId.of("Europe/Oslo");
+
+                boolean pass = false;
+                TextView tv14 = (TextView) findViewById(R.id.textView14);
+                TextView tv16 = (TextView) findViewById(R.id.textView16);
+                TextView tv15 = (TextView) findViewById(R.id.textView15);
+                TextView tv17 = (TextView) findViewById(R.id.textView17);
+                TextView tv18 = (TextView) findViewById(R.id.textView18);
+
                 for (int i = 0 ; i< us.run.size() ; i++)
                 {
                     if (us.run.get(i) == null)
                         continue;
-                    System.out.println(us.run);
-                    ZoneId zoneId = ZoneId.systemDefault(); // or: ZoneId.of("Europe/Oslo");
+
                     long course = LocalDateTime.parse(us.run.get(i).getDate()).atZone(zoneId).toEpochSecond();
                     long mtn = LocalDateTime.now().atZone(zoneId).toEpochSecond();
 
                     if (course > mtn)
                     {
                         TextView tv9 = (TextView) findViewById(R.id.textView9);
-                        long diff = course - mtn;
-                        if (diff> 24 && diff < 48)
+                        long diff = (course - mtn)/60/60;
+
+                        if (diff>= 24 && diff <= 48)
                         {
-                            tv9.setText(diff / 60 / 60/24 + " day");
+                            tv9.setText(diff /24 + " day");
                         }
                         else if (diff >48)
                         {
-                            tv9.setText(diff / 60 / 60/24 + " days");
+                            tv9.setText(diff /24 + " days");
                         }
                         else {
-                            tv9.setText(diff / 60 / 60 + " hours");
+                            tv9.setText(diff + " hours");
                         }
+                        System.out.println(tv9.getText());
+
+
+                        if (i>0) {
+                            pass = true;
+                            Run before = us.run.get(i - 1);
+
+                            tv14.setText(before.distance / 1000 + " km");
+                            tv16.setText(before.calories + " cal");
+                            tv15.setText(before.hundredthSecs / 1000/60 + " min");
+                            tv17.setText(before.pace + " min/km");
+                            tv18.setText(before.height + " m");
+                        }
+
+
                         break;
                     }
                 }
+
+                if(us.run.size()==0 || !pass)
+                {
+                    tv14.setText("0 km");
+                    tv16.setText("0 cal");
+                    tv15.setText("0 min");
+                    tv17.setText("0 min/km");
+                    tv18.setText("0 m");
+                }
+
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
@@ -78,6 +121,13 @@ public class MainScreenActivity extends AppCompatActivity {
             }
         };
         reference.addListenerForSingleValueEvent(postListener);
+
+        Button planrace = this.findViewById(R.id.button);
+        planrace.setOnClickListener(v -> {
+                Intent i = new Intent(this, activity_race_prog.class);
+                startActivity(i);
+        });
+
 
     }
 
